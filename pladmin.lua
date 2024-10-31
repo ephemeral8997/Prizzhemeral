a0a5ac25a85b26fb87b0efd5b2c26c84f6e86508
ephemeral8997 <53391982+ephemeral8997@users.noreply.github.com>
2024-10-31 15:33:02 +0300

    Update pladmin.lua

--- pladmin.lua
+++ pladmin.lua
@@ -21,6 +21,6 @@ PLadmin_Settings = {
   RankedOutput = true,
   WhisperToRanked = true
 }
-wait(120) -- Runtime issue
+wait(0.1) -- Runtime issue
 Execution_Runtime = tick()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/ephemeral8997/Prizzhemeral/refs/heads/main/Source/release_v0.8.1.lua'))()
